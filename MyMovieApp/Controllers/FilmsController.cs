35d4a9b82b3b326e4edac91a3dddad481b59ad96
Радченко Катерина <ketrinradchenko@gmail.com>
2023-12-20 18:00:21 +0200

    upload and validation were added

--- MyMovieApp/Controllers/FilmsController.cs
+++ MyMovieApp/Controllers/FilmsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,10 +13,12 @@ namespace MyMovieApp.Controllers
     public class FilmsController : Controller
     {
         private readonly FilmContext _context;
+        private readonly IWebHostEnvironment _appEnvironment;
 
-        public FilmsController(FilmContext context)
+        public FilmsController(FilmContext context, IWebHostEnvironment appEnvironment)
         {
             _context = context;
+            _appEnvironment = appEnvironment;
         }
 
         // GET: Films
@@ -53,16 +56,43 @@ namespace MyMovieApp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name,Director,Genre,Year,Actors,Description,Image")] Film film)
+        public async Task<IActionResult> Create
+            ([Bind("ID,Name,Director,Genre,Year,Actors,Description,Image")] Film film, 
+            IFormFile uploadedFile)
         {
+           
+            if (FilmExists3(film.Name, film.Director))
+                ModelState.AddModelError("", "Комбинация такого названия и режиссера уже есть!" +
+                    " Введите другое название, или другого режиссера.");
+
             if (ModelState.IsValid)
             {
+                if (uploadedFile != null)
+                {
+                    string path = "/img/" + uploadedFile.FileName;
+                    using (FileStream? fs = new FileStream
+                        (_appEnvironment.WebRootPath + path, FileMode.Create))
+                    {
+                        await uploadedFile.CopyToAsync(fs);
+                        film.Image = path;
+                    }                  
+                }
                 _context.Add(film);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
+
             }
             return View(film);
         }
+
+
+        //[AcceptVerbs("Get", "Post")]
+        //public IActionResult CheckName(string name)
+        //{
+        //    if (FilmExists2(name))
+        //        return Json(false);
+        //    return Json(true);
+        //}
 
         // GET: Films/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -71,6 +101,7 @@ namespace MyMovieApp.Controllers
             {
                 return NotFound();
             }
+
 
             var film = await _context.Films.FindAsync(id);
             if (film == null)
@@ -85,15 +116,32 @@ namespace MyMovieApp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Director,Genre,Year,Actors,Description,Image")] Film film)
+        public async Task<IActionResult> Edit(int id, 
+            [Bind("ID,Name,Director,Genre,Year,Actors,Description,Image")] Film film,
+            IFormFile? uploadedFile)
         {
             if (id != film.ID)
             {
                 return NotFound();
             }
 
+            if (FilmExists3(film.Name, film.Director))
+                ModelState.AddModelError("", "Комбинация такого названия и режиссера уже есть!" +
+                    " Введите другое название, или другого режиссера.");
+
             if (ModelState.IsValid)
             {
+
+                if (uploadedFile != null)
+                {
+                    string path = "/img/" + uploadedFile.FileName;
+                    using (FileStream? fs = new FileStream
+                        (_appEnvironment.WebRootPath + path, FileMode.Create))
+                    {
+                        await uploadedFile.CopyToAsync(fs);
+                        film.Image = path;
+                    }
+                }
                 try
                 {
                     _context.Update(film);
@@ -151,6 +199,12 @@ namespace MyMovieApp.Controllers
         private bool FilmExists(int id)
         {
             return _context.Films.Any(e => e.ID == id);
+        }
+
+        // проверка, что существует фильм с такой комбинацией имени и режиссера
+        private bool FilmExists3(string name, string direct)
+        {
+            return _context.Films.Any(e => e.Name == name && e.Director == direct);
         }
     }
 }
--- MyMovieApp/Models/Film.cs
+++ MyMovieApp/Models/Film.cs
@@ -1,14 +1,32 @@
-﻿namespace MyMovieApp.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyMovieApp.Models
 {
     public class Film
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string? Name { get; set; }  // название фильма
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string? Director { get; set; }  // режиссер
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string? Genre { get; set; }  // жанр
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Range(1900, 2023, ErrorMessage = "Недопустимый год")]
         public int Year { get; set; }  // год
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string? Description { get; set; }  // описание
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string? Actors { get; set; }  // описание
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string? Image { get; set; }  // путь к картинке
     }
 }
